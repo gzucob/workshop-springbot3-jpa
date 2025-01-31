915ba045e6a58cd275d42a0fca9c7172e8728106
gzucob <brezoliin@gmail.com>
2025-01-30 23:00:49 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -61,5 +61,14 @@ public class TestConfig implements CommandLineRunner {
         Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
         productRepository.saveAll(java.util.List.of(product1, product2, product3, product4, product5));
+
+        product1.getCategories().add(category2);
+        product2.getCategories().add(category1);
+        product2.getCategories().add(category3);
+        product3.getCategories().add(category3);
+        product4.getCategories().add(category3);
+        product5.getCategories().add(category2);
+
+        productRepository.saveAll(java.util.List.of(product1, product2, product3, product4, product5));
     }
 }
